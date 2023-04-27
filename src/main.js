d3591d9e17de7a34bc9be3c4711e574c8963a32c
Malik Kotb <malik@Maliks-MBP.fritz.box>
2023-04-27 14:54:48 +0200

    scrolling behaviour

--- src/main.js
+++ src/main.js
@@ -13,14 +13,9 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRa7SaZJW2jxM8eb9xMzXAHYyyU9crHl4",
-  authDomain: "chat-app-b3645.firebaseapp.com",
-  databaseURL: "https://chat-app-b3645-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "chat-app-b3645",
-  storageBucket: "chat-app-b3645.appspot.com",
-  messagingSenderId: "348160066722",
-  appId: "1:348160066722:web:9448cad9ff1c43a13883fb"
+  
 };
+
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
